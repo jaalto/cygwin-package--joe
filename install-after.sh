797e9602addb6701adeec7aeb4ecb6e2059f7c12
Jari Aalto <jari.aalto@cante.net>
2009-02-05 11:22:03 +0200

    3.7 updates
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- install-after.sh
+++ install-after.sh
@@ -15,6 +15,36 @@ Cmd()
     "$@"
 }
 
+Relocate ()
+{
+    echo ">> Relocate etc/joe/doc"
+
+    dir=$root/etc/joe/doc
+
+    if [ -d $dir ] ; then
+	Cmd mv $root/etc/joe/doc/* $root/usr/share/doc/joe-*/
+	Cmd rmdir $dir
+    fi
+
+    echo ">> Relocate etc/joe/*rc"
+    docdir=$(cd $root/usr/share/doc/joe-*/ && pwd)
+    exdir=$docdir/examples
+    dir=$root/etc/joe
+
+    if [ -d $dir ] && [ "$docdir" ] ; then
+	Cmd install -d -m 755 $exdir
+	Cmd mv $dir/* $exdir/
+    fi
+}
+
+Delete ()
+{
+    # See joe.README, why this file is removed
+
+    list=$(find $root -name termidx.exe)
+    [ "$list" ] && Cmd rm --verbose $list
+}
+
 Main()
 {
     root=${1:-".inst"}
@@ -23,30 +53,7 @@ Main()
 
         root=$(echo $root | sed 's,/$,,')  # Delete trailing slash
 
-        # See joe.README, why this file is removed
-
-        list=$(find $root -name termidx.exe)
-        [ "$list" ] && Cmd rm --verbose $list
-
-        echo ">> Relocate etc/joe/doc"
-
-        dir=$root/etc/joe/doc
-
-        if [ -d $dir ] ; then
-            Cmd mv $root/etc/joe/doc/* $root/usr/share/doc/joe-*/
-            Cmd rmdir $dir
-        fi
-
-        echo ">> Relocate etc/joe/*rc"
-        docdir=$(cd $root/usr/share/doc/joe-*/ && pwd)
-        exdir=$docdir/examples
-        dir=$root/etc/joe
-
-        if [ -d $dir ] && [ "$docdir" ] ; then
-            Cmd install -d -m 755 $exdir
-            Cmd mv $dir/* $exdir/
-        fi
-
+	Delete
     fi
 }
 
