2d33108e8c9d9e095593bff4a8d3e34792d060d8
Jari Aalto <jari.aalto@cante.net>
2009-02-05 11:16:07 +0200

    import original CYGWIN-PATCHES 2009-02-05

---
+++ install-after.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+# install-after.sh -- Custom installation steps
+#
+# The script will receive one argument: relative path to
+# installation root directory. Script is called like:
+#
+#    $ ./install-after.sh .inst/
+#
+# This script is used to things after the [install] command.
+
+Cmd()
+{
+    echo "$@"
+    [ "$test" ] && return
+    "$@"
+}
+
+Main()
+{
+    root=${1:-".inst"}
+
+    if [ "$root"  ] && [ -d $root ]; then
+
+        root=$(echo $root | sed 's,/$,,')  # Delete trailing slash
+
+        # See joe.README, why this file is removed
+
+        list=$(find $root -name termidx.exe)
+        [ "$list" ] && Cmd rm --verbose $list
+
+        echo ">> Relocate etc/joe/doc"
+
+        dir=$root/etc/joe/doc
+
+        if [ -d $dir ] ; then
+            Cmd mv $root/etc/joe/doc/* $root/usr/share/doc/joe-*/
+            Cmd rmdir $dir
+        fi
+
+        echo ">> Relocate etc/joe/*rc"
+        docdir=$(cd $root/usr/share/doc/joe-*/ && pwd)
+        exdir=$docdir/examples
+        dir=$root/etc/joe
+
+        if [ -d $dir ] && [ "$docdir" ] ; then
+            Cmd install -d -m 755 $exdir
+            Cmd mv $dir/* $exdir/
+        fi
+
+    fi
+}
+
+Main "$@"
+
+# End of file
